6e643eccc8e15f8ace4aa9756679fa451cd3cc1f
erg0dic <khalidmi@cardiff.ac.uk>
2025-10-26 23:40:09 +0000

    Deploying to gh-pages from @ erg0dic/erg0dic.github.io@f3e9cb3cc74ced58d6aeff3f4de9e5e32a61836b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-talks",
+          title: "talks",
+          description: "You can find below some of my invited talks and presentations.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/talks/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
